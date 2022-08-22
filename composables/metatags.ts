13616fb72d9b865d0351632ef62d79bb2fdcdb3f
Christopher Hill <cjameshill@gmail.com>
2022-08-22 11:18:24 +0000

    Update metatags.ts

--- composables/metatags.ts
+++ composables/metatags.ts
@@ -1,8 +1,7 @@
-const description =
-    "Be the first to get access and start giving";
+const description = "Be the first to get access and start giving";
 const title = "SndJoy - Get started and Pre Register";
-const primaryUrl = "https://start.gettonto.com/";
-const metaTagImage = "https://start.gettonto.com/secureYourHandleAtTonto.png";
+const primaryUrl = "https://start.sndjoy.com/";
+const metaTagImage = "https://start.sndjoy.com/secureYourHandleAtTonto.png";
 export function useMetaTags() {
     return { description, title, primaryUrl, metaTagImage };
 }
