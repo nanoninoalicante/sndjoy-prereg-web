42d856bf8646647330ff73116c1aca273d95648a
Christopher Hill <cjameshill@gmail.com>
2022-08-26 13:56:47 +0000

    Removed google tag script

--- app.vue
+++ app.vue
@@ -13,36 +13,6 @@ definePageMeta({
         name: "page",
     },
 });
-useHead({
-    title: "Secure your handle now - PreReg with Tonto now.",
-    viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-    charset: "utf-8",
-    script: [
-        {
-            hid: "fbpixel",
-            children: `!function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '3238823119770267');
-              fbq('track', 'PageView');`,
-            type: "text/javascript",
-        },
-        {
-            hid: "gtag",
-            children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TV653HH');`,
-            type: "text/javascript",
-        },
-    ],
-});
 </script>
 
 <style>
